4f0c70494d761801277e5003bdff872459256a96
nmyagistu <nikita.myagistu@gmail.com>
2026-02-04 17:06:03 +0200

    added LiveTransportRenderer

---
+++ src/components/MapContents/LiveTransportRenderer.tsx
@@ -0,0 +1,78 @@
+import 'leaflet/dist/leaflet.css';
+import { useEffect, useState } from 'react';
+import { CircleMarker, Marker, Popup, Tooltip } from 'react-leaflet';
+import { serverBaseUrl } from '../../const/envVariables';
+import { useMapBoundsContext } from '../../context/MapBoundsContext';
+import { liveTransportQueryMock } from '../../mocks/liveTransportQueryMock';
+import { LiveVehicleInfo } from '../../types/data';
+import { getRealtimeMarkerColor } from '../../utils';
+import { getTransportIcon } from '../../utils/dynamicSvg';
+import { processTransportLocationsTest } from '../../utils/fetch';
+
+import styles from './LiveTransportRenderer.module.css';
+
+export default function LiveTransportRenderer() {
+  const [transportMarkers, setTransportMarkers] = useState<LiveVehicleInfo[]>(
+    [],
+  );
+  const { filterMarkers } = useMapBoundsContext();
+  const liveTransportLineMock = `${liveTransportQueryMock.type}-${liveTransportQueryMock.lineNumber}`;
+
+  useEffect(() => {
+    const eventSource = new EventSource(
+      `${serverBaseUrl}/liveData/route/${liveTransportLineMock}`,
+    );
+
+    eventSource.onmessage = (event) => {
+      setTransportMarkers(processTransportLocationsTest(event.data));
+    };
+
+    return () => {
+      eventSource.close();
+    };
+  }, [liveTransportLineMock]);
+
+  const filteredMarkers = filterMarkers(transportMarkers);
+
+  return (
+    <>
+      {filteredMarkers.map(({ lat, lon, headingDegrees, endStopName }) => {
+        const { primaryColor, secondaryColor } = getRealtimeMarkerColor(
+          liveTransportQueryMock.type,
+        );
+        const icon = getTransportIcon(primaryColor, headingDegrees);
+
+        return (
+          <CircleMarker
+            center={[lat, lon]}
+            radius={15}
+            opacity={0.7}
+            color={primaryColor}
+            fillColor={secondaryColor}
+            fillOpacity={1}
+            key={`${liveTransportLineMock}-${lat}-${lon}`}
+          >
+            <Marker position={[lat, lon]} icon={icon}></Marker>
+            <Tooltip
+              direction='top'
+              opacity={1}
+              className={styles.tooltip}
+              permanent
+            >
+              {liveTransportQueryMock.lineNumber}
+            </Tooltip>
+            <Popup>
+              <strong>
+                {liveTransportLineMock} - {endStopName}
+              </strong>
+              <ul>{liveTransportQueryMock.type}</ul>
+              <ul>{lat}</ul>
+              <ul>{lon}</ul>
+              <ul>{headingDegrees}</ul>
+            </Popup>
+          </CircleMarker>
+        );
+      })}
+    </>
+  );
+}
